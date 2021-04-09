bfcad2f63bc424cc21452e843827db8245733b8f
unknown <guillaume.levy@student-cs.fr>
2021-04-09 16:50:48 +0200

    fin 9/04

--- Test/ppo_fgsm_regression/test_ppo_attack.py
+++ Test/ppo_fgsm_regression/test_ppo_attack.py
@@ -9,43 +9,44 @@ import matplotlib.pyplot as plt
 
 import fgsm
 
-def show(agent,eval_env,nb_episodes=10,nb_episodes_attaque=1,attack=False,model_fn=None,attack_function=None,render=False,targeted=False,y=None):
+def show(agent,eval_env,nb_episodes=10,nb_episodes_attaque=1,attack=False,model_fn=None,attack_function=None,render=False,targeted=False):
     episodes_rewards = []
     episode_reward = 0
-    pourcentage = 0
+    pourcentage = []
     episodes_pourcentages = []
-    prob_action = 0
+    prob_action = []
     episodes_prob_action = []
-    val_state = 0
+    episodes_prob_action_std = []
+    val_state = []
     episodes_val_state = []
     obs = eval_env.reset()
     eps_deb = 0
-    eps_fin = 0.3
+    eps_fin = 0.6
     k = 0
     steps = 0
     while k<nb_episodes*nb_episodes_attaque:
         #Convertir en un tenseur
         torch_obs = torch.Tensor(np.expand_dims(obs,axis=0))
-        action,_ = agent.predict(obs)
+        action,_ = agent.predict(obs,deterministic=True)
         if attack:
             if nb_episodes == 1:
-                eps = eps_fin
+                fact = eps_fin
             else:
-                eps = (eps_fin-eps_deb)/(nb_episodes-1)
+                fact = (eps_fin-eps_deb)/(nb_episodes-1)
             #Calculer l'image adversielle
-            obs_adv = attack_function(model_fn,torch_obs,eps_deb+eps*(k//nb_episodes_attaque),norm=np.inf,targeted=targeted,y=y)
-            #Calcuer la prédiction
-            action_adv,_ = agent.predict(obs_adv.detach())
+            eps = eps_deb+fact*(k//nb_episodes_attaque)
+            obs_adv = attack_function(model_fn,torch_obs,eps=eps,norm=np.inf,targeted=targeted)
+            action_adv,_ = agent.predict(obs_adv.detach(),deterministic=True)
             obs, reward, done, _ = eval_env.step(action_adv[0])
             if action != action_adv:
-                pourcentage = (1+pourcentage*steps)/(steps+1)
+                pourcentage.append(1)
+            else:
+                pourcentage.append(0)
 
         else:
-            obs, reward, done, _ = eval_env.step(action)
-        if model_fn == agent_actor:
-            prob_action = (((torch.max(agent_actor(torch_obs)-agent_actor(obs_adv)))+prob_action*steps)/(steps+1))
-        if model_fn == agent_critic:
-            val_state = (((torch.max(agent_critic(torch_obs)-agent_critic(obs_adv)))+val_state*steps)/(steps+1))
+            obs, reward, done, _ = eval_env.step(action.numpy()[0])
+        prob_action.append((torch.max(agent_actor(torch_obs)-agent_actor(obs_adv)).detach()))
+        val_state.append((torch.max(agent_critic(torch_obs)-agent_critic(obs_adv)).detach()))
         if render:
             eval_env.render()
         episode_reward += reward
@@ -53,15 +54,17 @@ def show(agent,eval_env,nb_episodes=10,nb_episodes_attaque=1,attack=False,model_
         if done:
             k += 1
             if k%nb_episodes_attaque==0:
+                print("Episode ",k//nb_episodes_attaque," sur ",nb_episodes)    
                 steps=0
                 episodes_rewards.append(episode_reward/nb_episodes_attaque)
                 episode_reward = 0
-                episodes_pourcentages.append(pourcentage)
-                pourcentage = 0
-                episodes_prob_action.append(prob_action)
-                prob_action = 0
-                episodes_val_state.append(val_state)
-                val_state = 0
+                episodes_pourcentages.append(np.mean(pourcentage))
+                pourcentage = []
+                episodes_prob_action.append(np.mean(prob_action))
+                episodes_prob_action_std.append(np.std(prob_action))
+                prob_action = []
+                episodes_val_state.append(np.mean(val_state))
+                val_state = []
             obs = eval_env.reset()   
 
     plt.subplot(221)
@@ -74,16 +77,15 @@ def show(agent,eval_env,nb_episodes=10,nb_episodes_attaque=1,attack=False,model_
     plt.xlabel('Puissance')
     plt.ylabel('Pourcentage')
     plt.legend(['fgsm_classification_untargeted','fgsm_classification_targeted','fgsm_regression_untargeted','fgsm_regression_targeted'])
-    if model_fn == agent_actor:
-        plt.subplot(223)
-        plt.plot([eps_deb+i*(eps_fin-eps_deb)/(nb_episodes-1) for i in range(len(episodes_prob_action))],episodes_prob_action,marker='o')
-        plt.xlabel('Puissance')
-        plt.ylabel('prob_action')
-    if model_fn == agent_critic:
-        plt.subplot(224)
-        plt.plot([eps_deb+i*(eps_fin-eps_deb)/(nb_episodes-1) for i in range(len(episodes_val_state))],episodes_val_state,marker='*')
-        plt.xlabel('Puissance')
-        plt.ylabel('val_state')
+    plt.subplot(223)
+    plt.plot([eps_deb+i*(eps_fin-eps_deb)/(nb_episodes-1) for i in range(len(episodes_prob_action))],episodes_prob_action,marker='o')
+    #plt.fill_between([eps_deb+i*(eps_fin-eps_deb)/(nb_episodes-1) for i in range(len(episodes_prob_action))],episodes_prob_action,episodes_prob_action_std)
+    plt.xlabel('Puissance')
+    plt.ylabel('prob_action')
+    plt.subplot(224)
+    plt.plot([eps_deb+i*(eps_fin-eps_deb)/(nb_episodes-1) for i in range(len(episodes_val_state))],episodes_val_state,marker='*')
+    plt.xlabel('Puissance')
+    plt.ylabel('val_state')
 
 
 #FGSM
@@ -114,11 +116,11 @@ if __name__ == '__main__':
     agent = PPO.load("PPO_Agent/model")
     plt.xlabel('Puissance')
     plt.ylabel('Rewards')
-    show(agent,env,nb_episodes_attaque=2,attack=True,model_fn=agent_actor,attack_function=fgsm.fast_gradient_method,targeted=False)
-    show(agent,env,nb_episodes_attaque=2,attack=True,model_fn=agent_actor,attack_function=fgsm.fast_gradient_method,targeted=True,y=torch.tensor([1]))
+    show(agent,env,nb_episodes_attaque=20,attack=True,model_fn=agent_actor,attack_function=fgsm.fast_gradient_method,targeted=False)
+    show(agent,env,nb_episodes_attaque=20,attack=True,model_fn=agent_actor,attack_function=fgsm.fast_gradient_method,targeted=True)
     
-    show(agent,env,nb_episodes_attaque=2,attack=True,model_fn=agent_critic,attack_function=fgsm.fast_gradient_method_regression,targeted=False)
-    show(agent,env,nb_episodes_attaque=2,attack=True,model_fn=agent_critic,attack_function=fgsm.fast_gradient_method_regression,targeted=True,y=torch.tensor([1,0],dtype=torch.float))
+    show(agent,env,nb_episodes_attaque=20,attack=True,model_fn=agent_critic,attack_function=fgsm.fast_gradient_method_regression,targeted=False)
+    show(agent,env,nb_episodes_attaque=20,attack=True,model_fn=agent_critic,attack_function=fgsm.fast_gradient_method_regression,targeted=True)
     
     plt.show()
 
